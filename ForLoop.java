0de409237a1f4ecb9450cbc421b6d0b73538fd49
Vinay Singh <vinaysingh1028@gmail.com>
2020-05-31 13:10:29 +0530

    For Loop in java

---
+++ ForLoop.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class ForLoop {
+	public static void  main(String[] args)
+	{
+		int count = 0;
+		for(int i=0; i<=.10; i++)
+		{
+			System.out.println(i);
+			count++;
+		}
+		System.out.println("The loop executed exactly "+count+" times");
+	}
+}
