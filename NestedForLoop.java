3fdbcd05cbd45fe59f1221ac87bd1f11cc24fc1e
Vinay Singh <vinaysingh1028@gmail.com>
2020-05-31 13:11:00 +0530

    Nested for Loop java

---
+++ NestedForLoop.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class NestedForLoop {
+	public static void main(String[] args)
+	{
+		for(int i=9; i>=0; i--)
+		{
+			for(int j=i; j>=0; j--)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
