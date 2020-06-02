214a6f059faf72bb2c1af3afeb2e6442a59cbff0
Vinay Singh <vinaysingh1028@gmail.com>
2020-06-02 12:36:32 +0530

    Sorting array elements using built in sort method

---
+++ ArraySortBuiltIn.java
@@ -0,0 +1,11 @@
+package practice;
+import java.util.Arrays;
+public class ArraySortBuiltIn {
+	public static void main(String[] args)
+	{
+		int[] arr = {55,10,12,66};
+		Arrays.sort(arr);
+		System.out.println(Arrays.toString(arr));
+		
+	}
+}
