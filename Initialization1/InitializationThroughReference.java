5502df4816e6193487fd1c3f52671fa2747faabd
Vinay Singh <vinaysingh1028@gmail.com>
2020-06-06 13:01:11 +0530

    initializing with reference var

---
+++ Initialization1/InitializationThroughReference.java
@@ -0,0 +1,13 @@
+package Initialization1;
+
+public class InitializationThroughReference {
+	public static void main(String[] args)
+	{
+		Student s = new Student();
+		s.id=10;
+		s.name="kuchh bhi";
+		System.out.println(s.id);
+		System.out.println(s.name);
+	}
+
+}
