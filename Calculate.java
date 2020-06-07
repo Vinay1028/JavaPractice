944459a75d28b17768cd2fa3b9728e161eda3e03
Vinay Singh <vinaysingh1028@gmail.com>
2020-06-07 17:23:45 +0530

    This is the simple program for calculation

---
+++ Calculate.java
@@ -0,0 +1,12 @@
+
+public class Calculate {
+	int x;
+	public static int cube(int x)
+	{
+		return x*x*x;
+	}
+	public static void main(String[] args) {
+		System.out.println(Calculate.cube(5));
+	}
+
+}
