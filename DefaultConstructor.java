437adfda66fea36288b8f3fe40431175a1a8fb73
Vinay Singh <vinaysingh1028@gmail.com>
2020-06-07 17:27:54 +0530

    Default constructor demo

---
+++ DefaultConstructor.java
@@ -0,0 +1,12 @@
+
+public class DefaultConstructor {
+	DefaultConstructor()
+	{
+		System.out.println("This is the default constructor");
+	}
+
+	public static void main(String[] args) {
+		DefaultConstructor d = new DefaultConstructor();
+	}
+
+}
