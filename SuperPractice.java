b4b3babbcfe3fb677939f301a42b87c6c5d5741b
Vinay Singh <vinaysingh1028@gmail.com>
2020-06-08 23:40:21 +0530

    Use of super keyword

---
+++ SuperPractice.java
@@ -0,0 +1,29 @@
+class Person{
+	int id;
+	String name;
+	Person(int id, String name)
+	{
+		this.id = id;
+		this.name = name;
+	}
+}
+class Emp extends Person{
+	float salary;
+	Emp(int id, String name, float salary)
+	{
+		super(id,name);
+		this.salary = salary;
+	}
+	void display() {
+		System.out.println(id+" "+name+" "+salary);
+	}
+}
+public class SuperPractice {
+
+	public static void main(String[] args) {
+		Emp e = new Emp(584,"Arun",586.0f);
+		e.display();
+
+	}
+
+}
