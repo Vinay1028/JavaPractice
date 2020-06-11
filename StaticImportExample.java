6a8c10ef53c41d9d8eba1cb447b12024bffdf2dd
Vinay Singh <vinaysingh1028@gmail.com>
2020-06-11 22:28:03 +0530

    with static import we can access static members of a class without qualifying with class name

---
+++ StaticImportExample.java
@@ -0,0 +1,11 @@
+import static java.lang.System.*;
+
+public class StaticImportExample {
+
+	public static void main(String[] args) {
+		out.println("my name is :");  	//no need to qualify the out with class System
+		out.println("singh");		//we can access all the static members of a class without qualifying it with class name
+
+	}
+
+}
